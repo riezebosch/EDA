3558114c72f261f1911108f7511015476832fc9e
Manuel Riezebosch <mriezebosch@gmail.com>
2021-02-15 14:22:57 +0100

    feat(event hubs): integration test processing messages

--- Basket.EventHubs.IntegrationTests/UnitTest1.cs
+++ Basket.EventHubs.IntegrationTests/UnitTest1.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections.Concurrent;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
 using Azure.Messaging.EventHubs.Producer;
+using Azure.Storage.Blobs;
+using Basket.Events;
+using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 
@@ -13,6 +19,8 @@ namespace Basket.EventHubs.IntegrationTests
         [Fact]
         public async Task Test1()
         {
+            var order = Generator.NewOrder();
+
             var config = new ConfigurationBuilder()
                 .AddUserSecrets<UnitTest1>()
                 .Build();
@@ -22,6 +30,21 @@ namespace Basket.EventHubs.IntegrationTests
             {
                 new EventData(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(Generator.NewOrder())))
             });
+
+            var messages = new BlockingCollection<EventData>();
+            var processor = new EventProcessorClient(new BlobContainerClient(config["Azure:EventHubs:StorageConnectionString"], "test"), EventHubConsumerClient.DefaultConsumerGroupName, config["Azure:EventHubs:ConnectionString"], "orders");
+            processor.ProcessEventAsync += async e =>
+            {
+                messages.Add(e.Data);
+                await e.UpdateCheckpointAsync();
+            };
+
+            processor.ProcessErrorAsync += e => throw e.Exception;
+            await processor.StartProcessingAsync();
+
+            messages.TryTake(out var result, TimeSpan.FromSeconds(30)).Should().BeTrue();
+            JsonSerializer.Deserialize<OrderCreated>(result!.EventBody).Should().BeEquivalentTo(order);
+            
         }
     }
 }
